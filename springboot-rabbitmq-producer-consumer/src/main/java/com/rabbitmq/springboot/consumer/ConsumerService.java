71ac5bd6a1eaa2b2c79eeff2325270562b8aa211
tom_glb <tom_glb@atguigu.com>
2021-10-11 12:36:40 +0800

    update file

--- springboot-rabbitmq-producer-consumer/src/main/java/com/rabbitmq/springboot/consumer/ConsumerService.java
+++ springboot-rabbitmq-producer-consumer/src/main/java/com/rabbitmq/springboot/consumer/ConsumerService.java
@@ -16,21 +16,28 @@ public class ConsumerService {
     @Autowired
     private RedisTemplate redisTemplate;
 
-   // @RabbitListener(queues = {"springboot-queue"})
+   @RabbitListener(queues = {"springboot-queue"})
     public void receiveMsg(Channel channel, String msg, Message message) throws IOException {
         Object messageId = message.getMessageProperties().getHeader("spring_returned_message_correlation");
         String messageIdStr = messageId.toString();
         String value = (String) redisTemplate.opsForValue().get(messageIdStr);
         if(!"1".equals(value)){
-            //说明数据还没有插入到数据库
-            //dao.insert();
-            //redis记录下该消息已经被消费
-            redisTemplate.opsForValue().set(messageIdStr,"1");
-            //手动ACK失败
-            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+
+            try {
+                //说明数据还没有插入到数据库
+                //dao.insert();
+                //redis记录下该消息已经被消费
+                redisTemplate.opsForValue().set(messageIdStr,"1");
+                //手动ACK失败
+                channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+            } catch (IOException e) {
+                channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,true);
+            }
+
         }else{
             System.out.println("收到消息==========================="+msg);
             channel.basicAck(message.getMessageProperties().getDeliveryTag(),false); //手动ACk的两个参数  第一个为消息的index，第二个表示是否批量确认消息
         }
+
     }
 }
